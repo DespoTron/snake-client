fe65adbe4d11cdf270495ee974b6668bd9fc5cdd
Thai Do <thai.hl.do@gmail.com>
2020-08-26 20:12:45 +0000

    refactored play.js

--- play.js
+++ play.js
@@ -1,5 +1,15 @@
-//const net = require('net');
 // play.js
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+
+
+
 console.log('Connecting ...');
 connect();
+
+setupInput();
