2a2a13fffb905ebffce65b2eeaeb475bbfb27448
Thai Do <thai.hl.do@gmail.com>
2020-08-26 20:18:19 +0000

    added info to handleUserInput

--- input.js
+++ input.js
@@ -1,6 +1,7 @@
 let connection;
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -12,8 +13,16 @@ const setupInput = function() {
 };
 
 const handleUserInput = key => {
-  if(key === '\u0003') {
+  if (key === '\u0003') {
     process.exit();
+  } else if (key === "w") {
+    console.log("up");
+  } else if (key === "a") {
+    console.log("left");
+  } else if (key === "s") {
+    console.log("down");
+  } else if (key === "d") {
+    console.log("right");
   }
 }
 
