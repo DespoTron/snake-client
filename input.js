c03091a32077063717f24c309437ff6800fdb063
Thai Do <thai.hl.do@gmail.com>
2020-08-26 20:13:26 +0000

    created input.js to process are movements

---
+++ input.js
@@ -0,0 +1,22 @@
+let connection;
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  stdin.on('data', handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = key => {
+  if(key === '\u0003') {
+    process.exit();
+  }
+}
+
+setupInput();
+
+module.exports = { setupInput };
